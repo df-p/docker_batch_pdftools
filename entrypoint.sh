e150b30e323394d969689ca302affbfcef8b9eda
Tiryoh <tiryoh@gmail.com>
2019-07-19 17:52:11 +0900

    Fix GID and UID when not specified by args

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 USER_ID=${UID:-1001}
 GROUP_ID=${GID:-1001}
 
-echo "Starting with UID : $UID, GID: $GID"
-addgroup -g ${GID} user
-adduser -D -u ${UID} -G user user
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+addgroup -g ${GROUP_ID} user
+adduser -D -u ${USER_ID} -G user user
 export HOME=/home/user
 
 exec /sbin/su-exec user "$@"
